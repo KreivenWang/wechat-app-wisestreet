6582dab61d69738b4592b941dc5a52821953f1a0
Kreiven <Kreiven.wang@hotmail.com>
2018-01-10 22:52:52 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,27 @@
+//index.js
+
+// Page({
+//   data: {
+//     motto: 'Hello World',
+    
+//   },
+//   //事件处理函数
+//   bindViewTap: function() {
+//     wx.navigateTo({
+//       url: '../logs/logs'
+//     })
+//   },
+//   onLoad: function () {
+    
+//   },
+  
+// })
+
+Page({
+  switch1Change: function (e) {
+    console.log('switch1 发生 change 事件，携带值为', e.detail.value)
+  },
+  switch2Change: function (e) {
+    console.log('switch2 发生 change 事件，携带值为', e.detail.value)
+  }
+})
